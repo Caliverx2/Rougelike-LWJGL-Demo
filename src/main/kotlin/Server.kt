e12cb2618ccfbb145890439ae6f7276fcaff86e3
Caliverx2 <mordaplutatv.yt@gmail.com>
2025-12-03 23:29:35 +0100

    bajlando v1

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -98,7 +98,7 @@ class ServerLogic(private val listener: ServerStatusListener) {
                 val messageType = parts[0]
                 val clientAddress = Pair(packet.address, packet.port)
 
-                if (messageType == "POSITION" && parts.size == 5) {
+                if (messageType == "POSITION" && parts.size == 5) { //AHAHAAJAJAJAJA
                     val isNewClient = !clients.containsKey(clientAddress)
 
                     val clientInfo = clients.computeIfAbsent(clientAddress) {
